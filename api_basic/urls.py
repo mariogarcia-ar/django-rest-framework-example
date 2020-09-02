b1053d2039a1fd4c70b476cd8985cf5cae81aefc
Mario Garcia <mariogarcia.ar@gmail.com>
2020-09-02 00:39:59 -0300

    fix list

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import GenericAPIView
 
-urlpatterns = [
+urlpatterns = [ 
+    path('generic/article/', GenericAPIView.as_view()),
     path('generic/article/<int:id>/', GenericAPIView.as_view()),
 ]
